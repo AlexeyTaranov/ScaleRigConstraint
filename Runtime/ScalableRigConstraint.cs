bbc88946128042536645c1a38f8a3ab85051db73
Alexey Taranov <alexey.taranov0@gmail.com>
2022-01-30 14:43:45 +0300

    Empty scalable rig implementation

---
+++ Runtime/ScalableRigConstraint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Animations.Rigging;
+
+namespace ScalableRig
+{
+    public class ScalableRigConstraint : RigConstraint<ScalableRigConstraintJob, ScalableRigConstraintJobData,
+        ScalableRigConstraintBinder>
+    {
+    }
+
+    [System.Serializable]
+    public struct ScalableRigConstraintJobData : IAnimationJobData
+    {
+        public bool IsValid()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetDefaultValues()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Runtime/ScalableRigConstraintJob.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Animations.Rigging;
+
+namespace ScalableRig
+{
+    public struct ScalableRigConstraintJob : IWeightedAnimationJob
+    {
+        public void ProcessAnimation(AnimationStream stream)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ProcessRootMotion(AnimationStream stream)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public FloatProperty jobWeight { get; set; }
+    }
+
+    public class
+        ScalableRigConstraintBinder : AnimationJobBinder<ScalableRigConstraintJob, ScalableRigConstraintJobData>
+    {
+        public override ScalableRigConstraintJob Create(Animator animator, ref ScalableRigConstraintJobData data,
+            Component component)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Destroy(ScalableRigConstraintJob job)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
