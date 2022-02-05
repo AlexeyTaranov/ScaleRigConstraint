1ea41ea33cb891d17e32f59269d9b05d00c29856
Alexey Taranov <alexey.taranov0@gmail.com>
2022-02-05 13:36:40 +0300

    Basic scalable rig implementation
    TODO Features:
    Generate order (select bones in editor, generate with clear childrens)
    Generate local offsets and scales
    Apply custom scale, offset local.

--- Runtime/ScalableRigConstraint.cs
+++ Runtime/ScalableRigConstraint.cs
@@ -1,23 +1,42 @@
-﻿using UnityEngine.Animations.Rigging;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace ScalableRig
 {
-    public class ScalableRigConstraint : RigConstraint<ScalableRigConstraintJob, ScalableRigConstraintJobData,
+    public class ScalableRigConstraint : RigConstraint<
+        ScalableRigConstraintJob,
+        ScalableRigConstraintJobData,
         ScalableRigConstraintBinder>
     {
+        private void OnValidate()
+        {
+            if (m_Data.Data.Length > 8)
+            {
+                Debug.LogError($"[{nameof(ScalableRigConstraint)}] Support blend only maximum 8 bones");
+                var arrayWithLimit = new TransferTransformOffsetLocalScale[8];
+                Array.Copy(m_Data.Data, arrayWithLimit, 8);
+                m_Data.Data = arrayWithLimit;
+            }
+        }
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct ScalableRigConstraintJobData : IAnimationJobData
     {
-        public bool IsValid()
-        {
-            throw new System.NotImplementedException();
-        }
+        public TransferTransformOffsetLocalScale[] Data;
+
+        public bool IsValid() => true;
 
         public void SetDefaultValues()
         {
-            throw new System.NotImplementedException();
         }
+    }
+
+    [Serializable]
+    public struct TransferTransformOffsetLocalScale
+    {
+        public Transform ToWrite;
+        public Transform FromRead;
     }
 }
--- Runtime/ScalableRigConstraintJob.cs
+++ Runtime/ScalableRigConstraintJob.cs
@@ -6,31 +6,52 @@ namespace ScalableRig
 {
     public struct ScalableRigConstraintJob : IWeightedAnimationJob
     {
+        public ReadOnlyTransformHandle[] ReadScale;
+        public ReadWriteTransformHandle[] WriteScale;
+        public FloatProperty jobWeight { get; set; }
+
         public void ProcessAnimation(AnimationStream stream)
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i < ReadScale.Length; i++)
+            {
+                var read = ReadScale[i];
+                var write = WriteScale[i];
+                write.SetLocalScale(stream, read.GetLocalScale(stream));
+            }
         }
 
         public void ProcessRootMotion(AnimationStream stream)
         {
-            throw new System.NotImplementedException();
         }
-
-        public FloatProperty jobWeight { get; set; }
     }
 
-    public class
-        ScalableRigConstraintBinder : AnimationJobBinder<ScalableRigConstraintJob, ScalableRigConstraintJobData>
+    public class ScalableRigConstraintBinder :
+        AnimationJobBinder<ScalableRigConstraintJob, ScalableRigConstraintJobData>
     {
         public override ScalableRigConstraintJob Create(Animator animator, ref ScalableRigConstraintJobData data,
             Component component)
         {
-            throw new System.NotImplementedException();
+            var arrayLength = data.Data.Length;
+            var readScales = new ReadOnlyTransformHandle[arrayLength];
+            var writeScales = new ReadWriteTransformHandle[arrayLength];
+            for (int i = 0; i < data.Data.Length; i++)
+            {
+                var pair = data.Data[i];
+                readScales[i] = ReadOnlyTransformHandle.Bind(animator, pair.FromRead);
+                writeScales[i] = ReadWriteTransformHandle.Bind(animator, pair.ToWrite);
+            }
+
+            var job = new ScalableRigConstraintJob
+            {
+                ReadScale = readScales,
+                WriteScale = writeScales,
+                jobWeight = FloatProperty.Bind(animator, component, "m_Weight"),
+            };
+            return job;
         }
 
         public override void Destroy(ScalableRigConstraintJob job)
         {
-            throw new System.NotImplementedException();
         }
     }
 }
